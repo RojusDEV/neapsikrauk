74e47ac942fd4f9349403b99038315e17506ed6e
Rojus <rojusbalciunas@gmail.com>
2026-02-04 23:19:09 +0200

    Feat: Added custom hook for formatting date

---
+++ src/hooks/FormatDate.ts
@@ -0,0 +1,8 @@
+const formatDate = (date: Date): string => {
+  const day = date.getDate().toString().padStart(2, "0");
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const year = date.getFullYear();
+  return `${year}-${month}-${day}`;
+};
+
+export default formatDate;
